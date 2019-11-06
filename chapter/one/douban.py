9f2d83a48b68514be91575b9abde07094cdefa4e
RH <52927295@qq.com>
2019-11-06 20:39:09 +0800

    #第一章：编程题（爬豆瓣首页并保存）

---
+++ chapter/one/douban.py
@@ -0,0 +1,12 @@
+import urllib.request  # 导入 urllib.request模块
+import ssl  # 导入ssl模块
+
+# 豆瓣
+douban_url = "https://www.douban.com/"
+# 把证书验证改成不用验证
+ssl._create_default_https_context = ssl._create_unverified_context
+# 保存网页
+douban_html_file = urllib.request.urlretrieve(douban_url, "douban.html")
+
+
+
